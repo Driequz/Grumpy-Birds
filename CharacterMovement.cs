b9cbba64ce11ca743b4d25430fc1d0433827553b
Driequz <32025431+Driequz@users.noreply.github.com>
2019-04-22 13:32:35 -0700

    Code

---
+++ CharacterMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float joyspeed;
+    public float idlespeed;
+    public float sadnessspeed;
+   // public float surprisedspeed;
+
+    ImageResultsParser userEmotions;
+    Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("GameController").transform;
+        userEmotions = player.GetComponent<ImageResultsParser>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float thresh = 10;
+
+        if (userEmotions.joyLevel > thresh)
+        {
+
+            transform.Translate(Vector3.up * joyspeed * Time.deltaTime);
+
+        }
+
+        else if (userEmotions.sadnessLevel > thresh)
+        {
+            //setSad();
+            transform.Translate(-Vector3.forward * sadnessspeed * Time.deltaTime);
+        }
+
+        //else if (userEmotions.surpriseLevel > thresh)
+        //{
+        //    //setSad();
+        //    transform.Translate(-Vector3.back * surprisedspeed * Time.deltaTime);
+        //}
+
+        else
+        {
+            transform.Translate(Vector3.down * idlespeed * Time.deltaTime);
+        }
+
+
+    }
+}
---
+++ FacialExpressions.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacialExpressions : MonoBehaviour
+{
+
+    //Script containing levels of emotions
+    ImageResultsParser userEmotions;
+
+    //player Transform used to obtain reference to UserEmotions script
+    Transform player;
+
+    //Used to change the face from smiling to frowning
+    public Renderer faceRenderer;
+
+
+    private Material faceMaterial;
+    private Vector2 uvOffset;
+    private Animator animator;
+
+
+    // Initialization and Setting references
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("GameController").transform;
+        userEmotions = player.GetComponent<ImageResultsParser>();
+        uvOffset = Vector2.zero;
+        faceMaterial = faceRenderer.materials[1];
+
+    }
+
+
+    // Setting the user's dominant emotion every frame
+    void Update()
+    {
+
+        float thresh = 10;
+        float Sthresh = 20;
+        faceMaterial.SetTextureOffset("_MainTex", uvOffset);
+
+
+        //the Update() method finds the dominant emotion and calls the appropriate method to set the emotion on the character.
+        //Therefore, you must get the emotions of joy and sadness from the public variables in the ImageResultsParser class. 
+
+        //If the dominant emotion is less than some threshold, such as 40 (it is on a scale from 0-100), 
+        //then it is not strong enough so just set the character to be idle.
+
+        //psuedo
+
+
+
+        if (userEmotions.joyLevel > thresh)
+        {
+
+            setJoyful();
+
+        }
+
+        //else if (userEmotions.surpriseLevel > Sthresh)
+        //{
+        //    setSurprise();
+        //}
+
+        else if (userEmotions.sadnessLevel > thresh)
+        {
+            setSad();
+        }
+
+        else
+        {
+            setIdle();
+
+        }
+
+
+        // TODO for Question 2
+    }
+
+    //sets the Character's emotion to Idle (Emotionless)
+    void setIdle()
+    {
+
+        // TODO for Question 2
+
+        uvOffset = new Vector2(0, 0);
+    }
+
+    //sets the Character's emotion to Joyful (Smiling)
+    void setJoyful()
+    {
+        //   
+        // TODO for Question 2
+
+        uvOffset = new Vector2(0.2f, 0);
+
+
+    }
+
+    //sets the Character's emotion to Sad (Frowning)
+    void setSad()
+    {
+
+        // TODO for Question 2
+
+        uvOffset = new Vector2(0, -0.25f);
+    }
+
+    void setSurprise()
+    {
+
+        // TODO for Question 2
+
+        uvOffset = new Vector2(-0.1f, 0);
+    }
+}
